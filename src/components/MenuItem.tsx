48fcd14b01d926946f796603220dae57ee8ab514
Tosin-JD <oluwatosinjosephdurodola@gmail.com>
2025-03-07 21:01:19 +0100

    removed some bugs

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -9,7 +9,7 @@ interface MenuItemProps {
 const MenuItem: React.FC<MenuItemProps> = ({href="#", children }) => {
   return (
     <li className="p-2">
-        <a href="#" className="block p-2 rounded-md text-gray-700 hover:bg-gray-100 hover:text-gray-900 hover:font-bold">
+        <a href={href} className="block p-2 rounded-md text-gray-700 hover:bg-gray-100 hover:text-gray-900 hover:font-bold">
             {children}
         </a>
     </li>
